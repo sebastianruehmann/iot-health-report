ee1c3e027e26f386e70e1e9972861910858c36ae
Sebastian Rühmann <mail@sebastianruehmann.de>
2017-11-24 15:42:57 +0100

    🎉 created react app

---
+++ client/src/containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div className="App-content">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
